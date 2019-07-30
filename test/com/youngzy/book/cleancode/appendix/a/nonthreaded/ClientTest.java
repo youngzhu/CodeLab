5321cb7164565b2d54696f36060803bd41434f25
Young-Mac <young.zhu.2012@gmail.com>
2019-07-31 00:31:16 +0800

    客户端/服务器非线程实现

---
+++ test/com/youngzy/book/cleancode/appendix/a/nonthreaded/ClientTest.java
@@ -0,0 +1,95 @@
+package com.youngzy.book.cleancode.appendix.a.nonthreaded;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.youngzy.book.cleancode.appendix.a.MessageUtils;
+import com.youngzy.book.cleancode.appendix.a.Server;
+
+public class ClientTest {
+	private static final int PORT = 8009;
+	private static final int TIMEOUT = 2000;
+	
+	Server server;
+	Thread serverThread;
+	
+	@Before
+	public void createServer() {
+		try {
+			server = new Server(PORT, TIMEOUT);
+		} catch (IOException e) {
+			e.printStackTrace(System.err);
+		}
+		serverThread = new Thread(server);
+		serverThread.start();
+	}
+	
+	@Test(timeout = 10000)
+	public void shouldRunInUnder10Sec() throws InterruptedException {
+		Thread[] threads = new Thread[10];
+		
+		for (int i = 0; i < threads.length; ++i) {
+			threads[i] = new Thread(new TrivialClient(i));
+			threads[i].start();
+		}
+		
+		for (int i = 0; i < threads.length; ++i) {
+			threads[i].join();
+		}
+	}
+	
+	class TrivialClient implements Runnable {
+		int clientNumber;
+		
+		TrivialClient(int clientNumber) {
+			this.clientNumber = clientNumber;
+		}
+
+		@Override
+		public void run() {
+
+			try {
+				connectSendReceive(clientNumber);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		
+	}
+	
+	private void connectSendReceive(int clientNumber) throws Exception {
+
+		System.out.printf("Client %2d: connecting\n", clientNumber);
+		Socket socket = new Socket("localhost", PORT);
+		
+		System.out.printf("Client %2d: sending message\n", clientNumber);
+		MessageUtils.sendMessage(socket, Integer.toString(clientNumber));
+		
+		String msg = MessageUtils.getMessage(socket);
+		System.out.printf("Client %2d: getting reply: %s\n", clientNumber, msg);
+		
+		System.out.printf("Client %2d: finished\n", clientNumber);
+		socket.close();
+	}
+	
+	@After
+	public void shutdownServer() {
+		if (server != null) {
+			server.stopProcessing();
+			try {
+				serverThread.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	
+
+}
