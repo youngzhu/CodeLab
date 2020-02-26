81dba94a2e748f1132f65723e298cf7fedecc126
young-win <young@win.com>
2020-02-26 14:24:09 +0800

    Restore IP Addresses，回溯法

--- src/main/java/com/youngzy/leetcode/string/RestoreIPAddresses.java
+++ src/main/java/com/youngzy/leetcode/string/RestoreIPAddresses.java
@@ -13,7 +13,84 @@ import java.util.List;
  * 链接：https://leetcode-cn.com/problems/restore-ip-addresses
  */
 public class RestoreIPAddresses {
+
+    // 每个ip段的最大和最小长度
+    private int SEGMENT_LEN_MAX = 3;
+    private int SEGMENT_LEN_MIN = 1;
+    private int DEPTH = 4; // IP段数量（深度）
+
+    /**
+     * 回溯法
+     *
+     * @param s
+     * @return
+     */
     public List<String> restoreIpAddresses(String s) {
+        List<String> ans = new ArrayList<>();
+
+        // 小于最小长度或者 大于最大长度
+        if (s.length() < SEGMENT_LEN_MIN * DEPTH || s.length() > SEGMENT_LEN_MAX * DEPTH) {
+            return ans;
+        }
+
+        // 存放每一段IP，如["10", "10", "0", "1"]
+        String[] segmentArr = new String[DEPTH]; //
+        backtrack(ans, s, 1, 0, segmentArr);
+
+        return ans;
+    }
+
+    /**
+     * 回溯
+     */
+    private void backtrack(List<String> ans, String s, int depth, int startIdx, String[] segmentArr) {
+        String segment; // ip 段
+        String ip; // 完整的ip
+        for (int len = 1; len <= SEGMENT_LEN_MAX; len++) {
+            // 剩下长度
+            int overlen = s.length() - (startIdx + len);
+            // 剩下的长度小于最小长度或大于 最大长度，跳过
+            if (overlen < SEGMENT_LEN_MIN * (DEPTH - depth)
+                    || overlen > SEGMENT_LEN_MAX * (DEPTH - depth)) {
+                continue;
+            }
+
+//            System.out.println(startIdx + ", " + len);
+            segment = s.substring(startIdx, startIdx + len);
+
+            if (isValidSegment(segment)) {
+                segmentArr[depth - 1] = segment;
+                if (depth == DEPTH) {
+                    ip = String.join(".", segmentArr);
+                    ans.add(ip);
+                } else {
+                    backtrack(ans, s, depth + 1, startIdx + len, segmentArr);
+                }
+            }
+
+        }
+    }
+
+    /**
+     * 是否是一个有效的IP段
+     * 1. 数字不以0开头，如08
+     * 2. <= 255
+     *
+     * @param segment
+     * @return
+     */
+    private boolean isValidSegment(String segment) {
+        int ipNum = Integer.valueOf(segment);
+
+        // s + "", 跟 valueOf 效率大不一样
+        if (segment.length() == String.valueOf(ipNum).length()
+                && ipNum <= 255) {
+            return true;
+        }
+        return false;
+    }
+
+    public List<String> restoreIpAddresses1(String s) {
         List<String> list = new ArrayList<>();
 
         String a, b, c, d;
