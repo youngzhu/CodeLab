26b32e8cabecd13e923cbead3ae35dbc303a90fd
young-win <young@win.com>
2020-03-27 11:47:59 +0800

    LPR，利率拆分为 基准利率+浮动利率

--- src/test/java/com/youngzy/lpr/LPRTest.java
+++ src/test/java/com/youngzy/lpr/LPRTest.java
@@ -8,15 +8,19 @@ import java.math.BigDecimal;
 
 public class LPRTest {
 
-    private static final BigDecimal loanAmount = new BigDecimal(600000); // 贷款本金
-    private static final int loanTerm = 20; // 贷款年限
-    private static final BigDecimal rateOfYear = new BigDecimal(0.05537D); // 年利率
+    private static final BigDecimal LOAN_AMOUNT = new BigDecimal("600000"); // 贷款本金
+    private static final int LOAN_TERM = 20; // 贷款年限
+    private static final BigDecimal BASE_RATE = new BigDecimal("4.8"); // 基准利率（百分比，4.9%）
+    private static final BigDecimal FLOATING_RATE = new BigDecimal("0.737"); // 浮动利率
 
-    private LPR lpr = new LPR(loanAmount, loanTerm, rateOfYear);
+    private LPR lpr;
 
     @Before
     public void init() {
-
+        BigDecimal rate = BASE_RATE.add(FLOATING_RATE);
+        // 除以100
+        rate = rate.divide(new BigDecimal(100));
+        lpr = new LPR(LOAN_AMOUNT, LOAN_TERM, rate);
     }
 
 
