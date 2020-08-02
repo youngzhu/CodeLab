acbb90063a35b88c573710881b8970773c052610
Young-Mac <young.zhu.2012@gmail.com>
2020-08-02 17:43:41 +0800

    设计模式：生成器模式

---
+++ src/main/java/com/youngzy/stackskills/dp/p12builder/HTMLBuilder.java
@@ -0,0 +1,29 @@
+package com.youngzy.stackskills.dp.p12builder;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HTMLBuilder {
+    private static final String HEADER = "<html><body>";
+    private static final String FOOTER = "</body></html>";
+
+    private List<URL> urls = new ArrayList<>();
+
+    public void addURL(URL url) {
+        urls.add(url);
+    }
+
+    public String getHtml() {
+        StringBuilder sb = new StringBuilder(HEADER);
+
+        for (URL url : urls) {
+            String body = "<li><a href=\"" + url.getPath()
+                    + "\" target=\"blank\" a></li>";
+            sb.append("<br>" + body);
+        }
+
+        sb.append(FOOTER);
+        return sb.toString();
+    }
+}
---
+++ src/test/java/com/youngzy/stackskills/dp/p12builder/SQLQueryBuilderTest.java
@@ -0,0 +1,60 @@
+package com.youngzy.stackskills.dp.p12builder;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SQLQueryBuilderTest {
+
+    @Test
+    public void selectOneTable() {
+        String selectSQL = SQLQueryBuilder.newBuilder()
+                .queryType(SQLQueryBuilder.QueryType.SELECT)
+                .table("Person")
+                .selectColumns("name", "age", "gender")
+                .whereEquals("name", "Adam")
+                .whereEquals("gender", "m")
+                .getQuery();
+
+        String expected = "SELECT name, age, gender FROM Person WHERE name='Adam' AND gender='m'";
+        assertEquals(expected, selectSQL);
+    }
+
+    @Test
+    public void selectTwoTables() {
+        String selectSQL = SQLQueryBuilder.newBuilder()
+                .queryType(SQLQueryBuilder.QueryType.SELECT)
+                .table("Person", "p")
+                .table("Nation", "n")
+                .selectColumns("name", "age", "gender")
+                .whereEquals("p.name", "Adam")
+                .whereEquals("p.gender", "m")
+                .getQuery();
+
+        String expected = "SELECT name, age, gender FROM Person p, Nation n WHERE p.name='Adam' AND p.gender='m'";
+        assertEquals(expected, selectSQL);
+    }
+
+    @Test
+    public void selectNoWhere() {
+        String selectSQL = SQLQueryBuilder.newBuilder()
+                .queryType(SQLQueryBuilder.QueryType.SELECT)
+                .table("Person")
+                .selectColumns("name", "age", "gender")
+                .getQuery();
+
+        String expected = "SELECT name, age, gender FROM Person";
+        assertEquals(expected, selectSQL);
+    }
+
+    @Test
+    public void exception() {
+        String selectSQL = SQLQueryBuilder.newBuilder()
+                .table("Person")
+                .selectColumns("name", "age", "gender")
+                .getQuery();
+
+        String expected = "SELECT name, age, gender FROM Person";
+        assertEquals(expected, selectSQL);
+    }
+}
