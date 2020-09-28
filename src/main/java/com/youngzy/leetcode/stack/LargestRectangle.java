4a837f82b96bb5effcb41e2b57ef8a18b5f2e555
Young-Win <young@win.com>
2020-09-28 11:40:43 +0800

    LeetCode：84. 柱状图中最大的矩形：单调栈

--- src/main/java/com/youngzy/leetcode/stack/LargestRectangle.java
+++ src/main/java/com/youngzy/leetcode/stack/LargestRectangle.java
@@ -1,5 +1,8 @@
 package com.youngzy.leetcode.stack;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * 84. 柱状图中最大的矩形
  * 给定 n 个非负整数，用来表示柱状图中各个柱子的高度。每个柱子彼此相邻，且宽度为 1 。
@@ -11,6 +14,45 @@ package com.youngzy.leetcode.stack;
  * 链接：https://leetcode-cn.com/problems/largest-rectangle-in-histogram
  */
 public class LargestRectangle {
+
+    /**
+     * 学习别人的单调栈
+     * 关键点：
+     *  maxArea[i] = heights[i] * (right - left - 1)
+     *  right: 右边第一个小于 heights[i] 的值
+     *  left: 左边第一个小于 heights[i] 的值
+     *
+     * 测试结果：空间提升不多，但时间相差巨大
+     *  执行用时：10 ms, 在所有 Java 提交中击败了91.23% 的用户
+     *  内存消耗：39.7 MB, 在所有 Java 提交中击败了86.07% 的用户
+     *
+     * @param heights
+     * @return
+     */
+    public int largestRectangleArea(int[] heights) {
+        int answer = 0;
+
+        // 为了左右两边都有小值，原数组头尾各增加一个0
+        int[] newHeights = new int[heights.length + 2];
+        for (int i = 1; i < newHeights.length - 1; i ++) {
+            newHeights[i] = heights[i - 1];
+        }
+
+        Deque<Integer> stack = new LinkedList<Integer>();
+        // 必须全部遍历，这里的i是左右值，而不是当前值
+        for (int i = 0; i < newHeights.length; i ++) {
+            while (! stack.isEmpty() && newHeights[stack.peek()] > newHeights[i]) {
+                int cur = stack.pop();
+                int left = stack.peek();
+                int area = newHeights[cur] * (i - left - 1);
+                answer = Math.max(answer, area);
+            }
+
+            stack.push(i);
+        }
+
+        return answer;
+    }
     /**
      * 在上一版本的基础上优化一下循环代码
      *
@@ -19,7 +61,7 @@ public class LargestRectangle {
      * @param heights
      * @return
      */
-    public int largestRectangleArea(int[] heights) {
+    public int largestRectangleArea1(int[] heights) {
         int answer = 0;
         for (int start = 0; start < heights.length; start++) {
             int minHeight = heights[start]; // 最小的高度
