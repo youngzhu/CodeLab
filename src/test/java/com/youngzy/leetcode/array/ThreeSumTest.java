f1af2dd271df45d467c4fd3d65c148267d8bb828
Young-Win <young@win.com>
2021-04-16 11:11:41 +0800

    LeetCode 15. 三数之和

---
+++ src/test/java/com/youngzy/leetcode/array/ThreeSumTest.java
@@ -0,0 +1,31 @@
+package com.youngzy.leetcode.array;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class ThreeSumTest {
+
+    ThreeSum ts = new ThreeSum();
+
+    @Test
+    public void test() {
+        int[] input = {-1,0,1,2,-1,-4};
+
+        List<List<Integer>> result = ts.threeSum(input);
+
+        Integer[][] expected = {{-1,-1,2},{-1,0,1}};
+        Integer[][] actual = new Integer[result.size()][];
+        for (int i = 0; i < result.size(); i++) {
+            actual[i] = result.get(0).toArray(new Integer[0]);
+        }
+
+//        assertArrayEquals(expected, actual);
+        // 达不到 无序判断的效果
+        assertTrue(Arrays.deepEquals(expected, actual));
+    }
+
+}
