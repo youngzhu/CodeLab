749357133c743b2f0a4fc7313e760e7e2c7f118c
young-win <young.zhu.2012@gmail.com>
2024-09-27 16:24:53 +0800

    Groovy，集合

--- src/main/groovy/com/youngzy/groovy/HelloCollection.groovy
+++ src/main/groovy/com/youngzy/groovy/HelloCollection.groovy
@@ -140,10 +140,10 @@ assert [1, 2] + 3 + [4, 5] + 6 == [1, 2, 3, 4, 5, 6]
 // 等于调用 `plus` 方法
 assert [1, 2].plus(3).plus([4, 5]).plus(6) == [1, 2, 3, 4, 5, 6]
 
-def a = [1, 2, 3]
-a += 4      // 创建一个新列表，并以这种方式为 `a` 添加新的元素
-a += [5, 6]
-assert a == [1, 2, 3, 4, 5, 6]
+//def a = [1, 2, 3]
+//a += 4      // 创建一个新列表，并以这种方式为 `a` 添加新的元素
+//a += [5, 6]
+//assert a == [1, 2, 3, 4, 5, 6]
 
 assert [1, *[222, 333], 456] == [1, 222, 333, 456]
 assert [*[1, 2, 3]] == [1, 2, 3]
@@ -167,18 +167,288 @@ list[8] = 'x' // `[]` 可以当操作符使用，从而按需扩展列表
 // 如果需要，也可以在列表中添加 null 值
 assert list == ['a', 'b', 'z', 'e', 'u', 'v', 'g', null, 'x']
 
+/*
+删除
+ */
+assert ['a','b','c','b','b'] - 'c' == ['a','b','b','b']
+assert ['a','b','c','b','b'] - 'b' == ['a','c']
+assert ['a','b','c','b','b'] - ['b','c'] == ['a']
+
+list = [1,2,3,4,3,2,1]
+list -= 3           // 从原始列表中去除 `3` 创建一个新列表
+assert list == [1,2,4,2,1]
+assert ( list -= [2,4] ) == [1,1]
+
+list = [1,2,3,4,5,6,2,2,1]
+assert list.remove(2) == 3          // 去除第 3 个元素，然后返回该元素
+assert list == [1,2,4,5,6,2,2,1]
+
+// 如果你只想去除列表中第一个跟值相同的元素，而不是所有元素的话，可以使用 remove 方法
+list= ['a','b','c','b','b']
+assert list.remove('c')             // 去除 'c' 并返回 true，因为元素已经清除了
+assert list.remove('b')             // 去除第一个 'b' 并返回 true，因为元素已经清除了
+
+assert ! list.remove('z')           // 返回 false，因为没有这个元素可供清除
+assert list == ['a','b','b']
+
+list= ['a',2,'c',4]
+// 如果是数字就会被当做 index
+list.remove(2)
+assert list == ['a',2,4]
+list.clear()
+assert list == []
+
+/*
+ 集合上的操作
+ */
+assert 'a' in ['a','b','c']             // 如果某一元素属于该列表，则返回 true
+assert ['a','b','c'].contains('a')      // 等同于 Java 中的 `contains` 方法
+assert [1,3,4].containsAll([1,4])       // `containsAll` 将检查是否已经找到所有的元素
+
+assert [1,2,3,3,3,3,4,5].count(3) == 4  // 计算匹配相应值的元素的数目
+assert [1,2,3,3,3,3,4,5].count {
+    it%2==0                             // 计算符合谓语要求的元素数目
+} == 2
+
+// 交集
+assert [1,2,4,6,8,10,12,6].intersect([1,3,6,9,6,12]) == [1,6,6,12]
+
+// 没有交集
+assert [1,2,3].disjoint( [4,6,9] )
+assert ![1,2,3].disjoint( [2,4,6] )
+
+/*
+排序
+ */
+assert [6, 3, 9, 2, 7, 1, 5].sort() == [1, 2, 3, 5, 6, 7, 9]
+
+list = ['abc', 'z', 'xyzuvw', 'Hello', '321']
+assert list.sort {
+    it.size()
+} == ['z', 'abc', '321', 'Hello', 'xyzuvw']
+
+def listSort = [7, 4, -6, -1, 11, 2, 3, -9, 5, -13]
+assert listSort.sort {
+    a, b -> a == b ? 0 : Math.abs(a) < Math.abs(b) ? -1 : 1
+} == [-1, 2, 3, 4, 5, -6, 7, -9, 11, -13]
+
+mc = { a, b -> a == b ? 0 : Math.abs(a) < Math.abs(b) ? -1 : 1 }
+// 只适用于 JDK 8+
+listSort.sort(mc)
+assert listSort == [-1, 2, 3, 4, 5, -6, 7, -9, 11, -13]
+
+list3 = [6, -3, 9, 2, -7, 1, 5]
+
+Collections.sort(list3)
+assert list3 == [-7, -3, 1, 2, 5, 6, 9]
+
+Collections.sort(list3, mc)
+assert list3 == [1, 2, -3, 5, 6, -7, 9]
+
+/*
+ 复制
+ */
+assert [1, 2, 3] * 3 == [1, 2, 3, 1, 2, 3, 1, 2, 3]
+assert [1, 2, 3].multiply(2) == [1, 2, 3, 1, 2, 3]
+assert Collections.nCopies(3, 'b') == ['b', 'b', 'b']
+
+// JDK 的 nCopies 的语义跟列表所用的 multiply 截然不同
+assert Collections.nCopies(2, [1, 2]) == [[1, 2], [1, 2]] // 而不是 [1,2,1,2]
 
 
+/*
+ map
+ */
+def map = [name: 'Gromit', likes: 'cheese', id: 1234]
+assert map.get('name') == 'Gromit'
+assert map.get('id') == 1234
+assert map['name'] == 'Gromit'
+assert map['id'] == 1234
+assert map instanceof java.util.Map
 
+def emptyMap = [:]
+assert emptyMap.size() == 0
+emptyMap.put("foo", 5)
+assert emptyMap.size() == 1
+assert emptyMap.get("foo") == 5
 
+// Map 的键默认都是字符串：[a:1] 和 ['a':1] 是相等的。
+def a = 'Bob'
+def ages = [a: 43]
+assert ages['Bob'] == null // 没有找到 `Bob`
+assert ages['a'] == 43     // 因为 `a` 是一个字面量！
 
+ages = [(a): 43]            // 现在用括号把 `a` 转义
+assert ages['Bob'] == 43   // 就找到 `Bob` 了！
 
+/*
+ 对 map 的迭代
+ */
+//值得注意的是，使用 map 字面量标记定义的 map 都是有序的。也就是说，如果你迭代 map 中的元素，元素项绝对会按照它们添加进 map 的顺序返回。
+map = [
+        Bob  : 42,
+        Alice: 54,
+        Max  : 33
+]
 
+// `entry` 是一个 map 项
+//map.each { entry ->
+//    println "Name: $entry.key Age: $entry.value"
+//}
 
+// `entry` 是一个 map 项，`i` 是 map 中的索引
+//map.eachWithIndex { entry, i ->
+//    println "$i - Name: $entry.key Age: $entry.value"
+//}
 
+// 当然你还可以直接利用键与值来进行迭代
+//map.each { key, value ->
+//    println "Name: $key Age: $value"
+//}
 
+// Key、value 和 i 都可以作为 map 中的索引
+//map.eachWithIndex { key, value, i ->
+//    println "$i - Name: $key Age: $value"
+//}
 
+//另外值得一提的是，你永远不应使用 GString 作为 map 的键，因为 GString 的哈希值跟相应的 String 的哈希值是不同的。
+def key = 'some key'
+map = [:]
+def gstringKey = "${key.toUpperCase()}"
+map.put(gstringKey,'value')
+assert map.get('SOME KEY') == null
+assert map.get("SOME KEY") == null
+assert map.get(gstringKey) == 'value'
+assert map.get(gstringKey) == "value"
+//map.each { k, v ->
+//    println "key: $k, value: $v"
+//}
 
+/*
+过滤与搜索
+ */
+def people = [
+        1: [name:'Bob', age: 32, gender: 'M'],
+        2: [name:'Johnny', age: 36, gender: 'M'],
+        3: [name:'Claire', age: 21, gender: 'F'],
+        4: [name:'Amy', age: 54, gender:'F']
+]
 
+// 查找单独的一个项
+def bob = people.find {
+    it.value.name == 'Bob'
+}
+// 不能这么用
+//assert bob[age] == 32
+def females = people.findAll {
+    it.value.gender == 'F'
+}
 
+def ageOfBob = bob.value.age
+def agesOfFemales = females.collect {
+    it.value.age
+}
+assert ageOfBob == 32
+assert agesOfFemales == [21,54]
 
+def agesOfMales = people.findAll { id, person ->
+    person.gender == 'M'
+}.collect { id, person ->
+    person.age
+}
+assert agesOfMales == [32, 36]
+
+// 如果所有项都匹配谓语，`every` 就返回 true
+assert people.every { id, person ->
+    person.age > 18
+}
+
+// 如果所有项都匹配谓语，`any` 就返回 true
+assert people.any { id, person ->
+    person.age == 54
+}
+
+/*
+Grouping
+ */
+assert ['a', 7, 'b', [2, 3]].groupBy {
+    it.class
+} == [(String)   : ['a', 'b'],
+      (Integer)  : [7],
+      (ArrayList): [[2, 3]]
+]
+
+assert [
+        [name: 'Clark', city: 'London'],
+        [name: 'Sharma', city: 'London'],
+        [name: 'Maradona', city: 'LA'],
+        [name: 'Zhang', city: 'HK'],
+        [name: 'Ali', city: 'HK'],
+        [name: 'Liu', city: 'HK'],
+].groupBy {
+    it.city
+} == [
+        London: [[name: 'Clark', city: 'London'],
+                 [name: 'Sharma', city: 'London']],
+        LA    : [[name: 'Maradona', city: 'LA']],
+        HK    : [[name: 'Zhang', city: 'HK'],
+                 [name: 'Ali', city: 'HK'],
+                 [name: 'Liu', city: 'HK']],
+]
+
+/*
+范围（Ranges）
+
+用 .. 标记法定义的范围是全包含的（包含首尾两个值的列表）。
+
+用 ..< 标记法定义的范围是半包含的，即只含有起始值，而不包含末尾值。
+ */
+// 全包含范围
+def range = 5..8
+assert range.size() == 4
+assert range.get(2) == 7
+assert range[2] == 7
+assert range instanceof java.util.List
+assert range.contains(5)
+assert range.contains(8)
+
+// 半包含范围
+range = 5..<8
+assert range.size() == 3
+assert range.get(2) == 7
+assert range[2] == 7
+assert range instanceof java.util.List
+assert range.contains(5)
+assert !range.contains(8)
+
+//不使用索引，获取范围的末尾值
+range = 1..10
+assert range.from == 1
+assert range.to == 10
+
+range = 'a'..'d'
+assert range.size() == 4
+assert range.get(2) == 'c'
+assert range[2] == 'c'
+assert range instanceof java.util.List
+assert range.contains('a')
+assert range.contains('d')
+assert !range.contains('e')
+
+//for (i in 1..10) {
+//    println "Hello ${i}"
+//}
+
+//(1..10).each { i ->
+//    println "Hello ${i}"
+//}
+//(1..10).each {
+//    println "Hello ${it}"
+//}
+
+def years = 1
+switch (years) {
+    case 1..10: interestRate = 0.076; break;
+    case 11..25: interestRate = 0.052; break;
+    default: interestRate = 0.037;
+}
