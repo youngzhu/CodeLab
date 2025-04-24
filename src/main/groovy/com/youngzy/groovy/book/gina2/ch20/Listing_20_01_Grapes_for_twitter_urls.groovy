81b5e861a4eeba1d746cff3924e305edf3e7dc68
young-win <young.zhu.2012@gmail.com>
2025-04-24 12:05:48 +0800

    Groovy in Action, 2nd Edition, CH18: Concurrent Groovy with GPars

---
+++ src/main/groovy/com/youngzy/groovy/book/gina2/ch20/Listing_20_01_Grapes_for_twitter_urls.groovy
@@ -0,0 +1,12 @@
+package com.youngzy.groovy.book.gina2.ch20
+
+@Grab(group='org.ccil.cowan.tagsoup', module='tagsoup', version='1.2')
+import org.ccil.cowan.tagsoup.Parser
+
+def parser = new XmlParser(new Parser())
+def html = parser.parse("http://manning.com/koenig2")
+
+def twitterUrls = html.body.'**'.a.@href.grep(~/.*twitter.*/)
+println twitterUrls.join("\n")
+
+assert 'http://www.twitter.com/mittie' in twitterUrls
